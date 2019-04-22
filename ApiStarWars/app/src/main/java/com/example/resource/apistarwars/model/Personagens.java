9fec09c347620a2f7fcfd307a63ead09f412f5dc
Mateus <mateuslima565@gmail.com>
2019-04-22 17:22:51 -0300

    inicio

---
+++ ApiStarWars/app/src/main/java/com/example/resource/apistarwars/model/Personagens.java
@@ -0,0 +1,14 @@
+package com.example.resource.apistarwars.model;
+
+public class Personagens {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
